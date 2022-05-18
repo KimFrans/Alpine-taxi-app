79da08e1f15841d1fe687799d6de3903283295cd
Kim Frans <kimfrans34@gmail.com>
2022-05-18 15:37:05 +0200

    validation/no duplicates

--- index.js
+++ index.js
@@ -1,24 +1,103 @@
-console.log("hello");
-console.log(Alpine);
+// console.log("hello");
+
 document.addEventListener('alpine:init', () => {
-    console.log(Alpine);
+    // console.log(Alpine);
     Alpine.data('taxiData', () => ({
-        taxiData: [
+
+        // passengers=12,
+    
+
+        increment(taxiObject){
+            taxiObject.queue++
+        },
+        decrement(taxiObject){
+            taxiObject.queue--
+        },
+
+        addTaxi(taxiObject){
+            taxiObject.taxiAvail++
+        },
+
+        taxiData: Alpine.$persist([
             { destination: 'Makhaza', queue: 12, taxiCount: 0, fare: 12.00, taxiFareTotal: 0, taxiAvail: 3 },
             { destination: 'Belhar', queue: 34, taxiCount: 0, fare: 15.00, taxiFareTotal: 0, taxiAvail: 3 },
-            { destination: 'Bellville', queue: 23, taxiCount: 0, fare: 10.00, taxiFareTotal: 0, taxiAvail: 3 }]
+            { destination: 'Bellville', queue: 23, taxiCount: 0, fare: 10.00, taxiFareTotal: 0, taxiAvail: 3 }]),
+            
+
+
+            destinationExists(userDestination) {
+                return this.taxiData.some(function (element) {
+                    return element.destination === userDestination;
+                });
+            },
+
+            addDestination(userDestination, userFare, userQueue){
+                if(userDestination != ''){
+                    if(userFare != ''){
+                        if(userQueue != ''){
+                            if (this.destinationExists(userDestination) == false) {
+                                if (userDestination.match("^[a-zA-Z]*$")) {
+                                    // if(userFare.match("^[0-9]*$")){
+                                        // if(userQueue.match("^[0-9]*$")){
+                                            this.taxiData.push({destination:userDestination, queue:userQueue, taxiCount:0, fare:userFare, taxiFareTotal:0, taxiAvail:3})
+                                            userMessage = "The destination has been entered successfully"
+
+                                        // }else{
+                                        //     userMessage = "Please enter a valid queue"
+                                        // }
+            
+                                    // }else{
+                                    //     userMessage = "Please enter a valid fare amount"
+                                    // }
+                                }
+                                else if(!userDestination.match("^[a-zA-Z]*$")){
+                                  userMessage = "Please enter a valid destination name"
+                                }
+                            }
+                            else if (this.destinationExists(userDestination) == true) {
+                                userMessage = "The destination entered already exists"
+                            }
+
+                        }else{
+                            userMessage = "Missing queue number!"
+                        }
+
+                    }else{
+                        userMessage = "Missing fare amount!"
+                    }
+
+                }else{
+                    userMessage = "Missing destination!"
+                }
+
+            },
+
+            // returnMessage(){
+            //     return userMessage
+            // },
+
+
+            departTaxi(){
+                this.taxiData.queue=taxi.queue-12,
+                this.taxiData.taxiCount++,
+                this.taxiData.taxiFareTotal+=(taxi.fare*12),
+                this.taxiData.taxiAvail--
+            }, 
+
+            taxiFare(){
+                this.taxiData.fare
+            },
+
+            totalTaxiFare(){
+                this.taxiData.totalTaxiFare
+            },
+
+            taxiCountDepart(){
+                this.taxiData.taxiCount
+            }
+
     }))
 
 
 
-
 })
-// document.addEventListener('alpine:init', () => {
-//     Alpine.data('taxiData', () => ({
-//         open: false,
-
-//         toggle() {
-//             this.open = ! this.open
-//         }
-//     }))
-// })
